e993e4d7182b72cd4964a19fa88de364ab5a3307
hbbo0e <44goodluck44@gmail.com>
2024-12-30 19:03:56 +0900

    init

---
+++ src/main/java/gatherly/community/GatherlyCommunityApplication.java
@@ -0,0 +1,13 @@
+package gatherly.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherlyCommunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatherlyCommunityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/gatherly/community/GatherlyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package gatherly.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherlyCommunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
